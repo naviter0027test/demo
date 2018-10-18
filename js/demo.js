24f1fa328d4962e6e55a072ead156ebcd4430f02
lanker <lanker@test0060.axcell28.idv.tw>
2018-10-18 09:53:21 +0800

    add anchor js function finish

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,16 @@
+function scrollToAnchor(aid){
+    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+}
+
 $(document).ready(function() {
     $('.productContent').masonry({
         'itemSelector' : '.product',
         'animate' : true,
         'percentPosition' : true
+    });
+
+    $(".anchorDiv .anchor").on("click", function() {
+        scrollToAnchor($(this).attr("href"));
+        return false;
     });
 });
